db3e02b8a595484590e7c764d8a11e2498c6312c
bombxark27 <bjleungchoi@gmail.com>
2024-05-24 22:46:47 -0600

    1st draft of MemUserDAO (Phase3)

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,23 +1,36 @@
 package dataaccess;
 
 import model.UserData;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class MemoryUserDAO implements UserDAO {
+    private static Collection<UserData> users = new ArrayList<UserData>();
 
     @Override
-    public UserData createUser(String username, String password, String email) {
+    public UserData createUser(String username, String password, String email) throws DataAccessException{
+        for (UserData user : users){
+            if (user.username().equals(username)){
+                throw new DataAccessException("Username already exists");
+            }
+        }
         UserData user = new UserData(username, password, email);
-        return null;
+        users.add(user);
+        return user;
     }
 
     @Override
     public UserData getUser(String username) throws DataAccessException{
-
-        return null;
+        for (UserData user : users){
+            if (user.username().equals(username)){
+                return user;
+            }
+        }
+        throw new DataAccessException("User not found");
     }
 
     @Override
-    public UserData clear(){
-        return null;
+    public void clear(){
+        users.clear();
     }
 }
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -3,7 +3,7 @@ package dataaccess;
 import model.UserData;
 
 public interface UserDAO {
-    public UserData createUser(String username, String password, String email);
+    public UserData createUser(String username, String password, String email) throws DataAccessException;
     public UserData getUser(String username) throws DataAccessException;
-    public UserData clear();
+    public void clear();
 }
