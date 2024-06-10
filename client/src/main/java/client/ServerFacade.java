6136e20e6d3c7e0da5d1f1d80311decaacdab127
bombxark27 <bjleungchoi@gmail.com>
2024-06-09 18:42:20 -0600

    added functions in ServerFacade (Phase5)

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,4 +1,46 @@
 package client;
 
+import model.AuthData;
+
 public class ServerFacade {
+
+
+    public AuthData register(String username, String password, String email) {
+
+
+        return null;
+    }
+
+    public void login(String username, String password) {
+
+    }
+
+    public void logout() {
+
+    }
+
+    public void quit() {
+
+    }
+
+    public void help() {
+
+    }
+
+    public void createGame() {
+
+    }
+
+    public void listGames() {
+
+    }
+
+    public void playGame() {
+
+    }
+
+    public void observeGame() {
+
+    }
+
 }
