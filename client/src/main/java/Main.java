8e4cc8827a338b4c7d4635766637e55acd2e16bf
bombxark27 <bjleungchoi@gmail.com>
2024-06-12 18:05:07 -0600

    quality check3 (Phase5)

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,14 +1,12 @@
 import chess.*;
 
 import client.Repl;
-import client.ResponseException;
+
 public class Main {
 
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
-//        server = new Server();
-//        var port = server.run(0);
         var port = 8080;
         System.out.println("Started test HTTP server on " + port);
         String url = "http://localhost:" + port + "/";
@@ -16,6 +14,5 @@ public class Main {
             url = args[0];
         }
         new Repl(url).run();
-//        server.stop();
     }
 }
--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -5,6 +5,12 @@ public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
-        new Server().run(8080);
+        var port = 8080;
+        if (args.length >= 1) {
+            port = Integer.parseInt(args[0]);
+        }
+        new Server().run(port);
+        System.out.printf("Server started on port %d%n", port);
+
     }
 }
